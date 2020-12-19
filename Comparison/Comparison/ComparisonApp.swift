7ed76d34e901be72fec3a80c6a2015298f23e5e3
hosodahiroshi <applef_loveg_mea@icloud.com>
2020-12-19 11:18:09 +0900

    create project

---
+++ Comparison/Comparison/ComparisonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComparisonApp.swift
+//  Comparison
+//
+//  Created by 細田大志 on 2020/12/19.
+//
+
+import SwiftUI
+
+@main
+struct ComparisonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Comparison/Comparison/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Comparison
+//
+//  Created by 細田大志 on 2020/12/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
